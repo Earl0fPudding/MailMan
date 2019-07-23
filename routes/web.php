43a4b02ff01f3c7d183e2d328ddb5869e0d57b41
root <root@weidenauer.cc>
2019-07-23 11:32:36 +0200

    Add Laravel project

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'Login@showLogin')->name('Login.showLogin');
+Route::get('/start', 'Login@showStartPage')->name('Login.showStartPage');
+Route::post('/login', 'Login@login')->name('Login.login');
+Route::get('/logout', 'Login@logout')->name('Login.logout');
