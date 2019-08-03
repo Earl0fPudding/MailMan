baf2b0bf8231ce773bdd52fc497dcc86dc721445
Earl0fPudding <martin@weidenauer.cc>
2019-08-03 11:08:16 +0200

    Add material icons to invites page

--- resources/views/showinvites.blade.php
+++ resources/views/showinvites.blade.php
@@ -11,10 +11,10 @@
         <div class="container">
 	    <div class="row">
 		<div class="col m10">
-                    <h2>Invites</h2>
+                    <h3><i class="material-icons">person_add</i> Invites</h3>
 		</div>
 		<div class="col m2">
-			<a class="waves-effect waves-light modal-trigger add-button" href="#create-modal"><ion-icon size="large" name="add-circle"></ion-icon></a>
+		<a class="btn-floating btn-large waves-effect waves-light modal-trigger blue darken-2 add-button" href="#create-modal"><i class="material-icons">add</i></a>
 		</div>
 	    </div>
             <div class="row">
@@ -22,11 +22,11 @@
                     <table class="striped">
 			<thead>
 			    	<tr>
-				    <th>Invite link</th>
-				    <th>Name preset</th>
-				    <th>Domain</th>
-				    <th>Date of termination</th>
-				    <th>Options</th>
+				    <th><i class="material-icons tiny">link</i> Invite link</th>
+				    <th><i class="material-icons tiny">person</i> Name preset</th>
+				    <th><i class="material-icons tiny">language</i> Domain</th>
+				    <th><i class="material-icons tiny">access_time</i> Date of termination</th>
+				    <th><i class="material-icons tiny">build</i> Options</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -37,7 +37,7 @@
 					<td>{{ $invite->domain->name }}</td>
 					<td>{{ $invite->termination_date }} <!-- @if(strtotime($invite->terminaiton_date)>strtotime(date('Y-m-d H:i:s'))) <span class="green-text">Active</span> @else <span class="red-text">Expired</span> @endif --></td>
 					<td>
-					    <a href="{{route('Admin.deleteInvite', ['id' => $invite->id])}}"><button type="button" class="btn-flat">Delete</button></a>
+					    <a href="{{route('Admin.deleteInvite', ['id' => $invite->id])}}"><button type="button" class="btn-flat"><i class="material-icons">delete</i></button></a>
 					</td>
 				</tr>
 			    @endforeach
@@ -57,6 +57,7 @@
 	  <div class="container">
 	    <div class="row">
 	      <div class="input-field col m12">
+		<i class="material-icons prefix">language</i>
 		<select id="d_id" name="domain_id_add">
                   @if(sizeof($domains)==0) <option value="" disabled selected>No domains available</option> @endif
                   @foreach($domains as $domain)
@@ -68,16 +69,19 @@
 	    </div>
 	    <div class="row">
 	      <div class="input-field col m12">
+		<i class="material-icons prefix">person</i>
 		<input name="name_preset_add" value="{{ old('name_preset_add') }}" id="p_id" type="text" class="validate input-text @error('name_preset_add') invalid @enderror" data-length="50">
                 <label for="p_id">Name preset (optional)</label>
 	      </div>
 	    </div>
 	    <div class="row">
 	      <div class="input-field col m6">
+		<i class="material-icons prefix">date_range</i>
 		<input type="text" @if(old('termination_date_add')) value="{{old('termination_date_add')}}" @else value="{{ date('Y-m-d') }}" @endif class="datepicker @error('termination_date_add') invalid @enderror" name="termination_date_add" id="td_id" required>
 		<label for="td_id">Date of termination</label>
 	      </div>
 	      <div class="input-field col m6">
+		<i class="material-icons prefix">access_time</i>
                 <input type="text" value="{{old('termination_time_add')}}" class="timepicker @error('termination_time_add') invalid @enderror" name="termination_time_add" id="tt_id" required>
                 <label for="tt_id">Time of termination</label>
 	      </div>
@@ -85,7 +89,7 @@
 	  </div>
     </div>
     <div class="modal-footer" style="text-align:center;">
-      <button type="submit" href="#!" class="waves-effect waves-light btn blue darken-2">Add</button>
+      <button type="submit" href="#!" class="waves-effect waves-light btn blue darken-2"><i class="material-icons right">save</i>Add</button>
     </div>
     </form>
   </div>
@@ -118,5 +122,6 @@ $(document).ready(function(){
   });
 
 </script>
+<script src="{{asset('js/mailman.js')}}"></script>
     </body>
 </html>
